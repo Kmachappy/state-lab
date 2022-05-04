fbec2d12e1202fdf547d7b36624c7bb9ba7d0600
Alejandro <95323749+Kmachappy@users.noreply.github.com>
2022-05-04 03:11:08 -0700

    add green border on selected image

--- src/App.js
+++ src/App.js
@@ -1,32 +1,31 @@
-// IMPORT useState
 import { useState } from "react";
 import React from "react";
 import "./styles.css";
 import imagesArr from "./imageData";
-// IMPORT imageData.js AND STORE IT IN A VARIABLE CALLED imagesArr
 
 export default function App() {
-  // USE useState TO CREATE  [bigImage, setBigImage]
   const [bigImage, setBigImage] = useState(imagesArr[0].img);
-  // AND SET IT TO THE IMAGE URL OF THE FIRST ELEMENT IN THE ARRAY
 
-  // CREATE A HANDLE CLICK FUNCTION THAT ACCEPTS AN IMAGE URL
   const handleClick = (imgUrl) => {
-    // THE FUNCTION SHOULD CALL setBigImage AND PASS IT THE URL
     setBigImage(imgUrl);
   };
 
-  // CREATE A VARIABLE CALLED images THAT LOOPs OVER THE imagesArr AND RETURNS AN <IMG> ELEMENT
-  // ASSIGN ALL OF THE PROPERTIES THAT IT NEEDS: src, alt, className, key INCLUDING AN onClick EVENT THAT CALLS THE HANDLE EVENT FUNCTION AND PASSES IT THE IMG URL
   const images = imagesArr.map((ele, idx) => {
     return (
-      <img
-        src={ele.img}
-        alt={ele.city}
-        className="thumb"
-        key={idx}
-        onClick={() => handleClick(ele.img)}
-      />
+      console.log(bigImage),
+      (
+        <img
+          src={ele.img}
+          alt={ele.city}
+          // className="thumb"
+          // this way changes the class of the element and applies styling from css based on condition
+          className={ele.img !== bigImage ? "thumb" : "thumb border"}
+          key={idx}
+          onClick={() => handleClick(ele.img)}
+          // this way add styling inline to the element based on conditino. why does it leave and empty style once unclicked tho
+          // style={ele.img !== bigImage ? {} : { border: "2px solid green" }}
+        />
+      )
     );
   });
 
@@ -35,8 +34,6 @@ export default function App() {
       <h1>Cities Of The World</h1>
       <div id="wrapper">
         <div id="thumbnails">{images}</div>
-        {/* THE SRC IMAGE URL SHOULD BE SET TO THE VALUE THAT IS STORED IN bigImage */}
-        {/* <img src="" id="bigimage" alt='bigImage'/> */}
         <img src={bigImage} id="bigimage" alt="bigImage" />
       </div>
     </div>
--- src/Thumbnail.jsx
+++
@@ -1,9 +0,0 @@
-const Thumb = (
-  <img
-    class="thumb"
-    src="https://images.unsplash.com/photo-1534351590666-13e3e96b5017?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
-    alt="amsterdam"
-  />
-);
-
-export default Thumb;
